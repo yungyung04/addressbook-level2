cbe9f7b79dae871748d1a8095d6fa0e388c8ee3d
USER-PC\User <ekabuyung@gmail.com>
2018-02-15 11:30:39 +0800

    handling I/O test issue (not solved yet)

---
+++ test/runtests2.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+REM create bin directory if it doesn't exist
+if not exist ..\bin mkdir ..\bin
+
+REM delete output from previous run
+del actual.txt
+
+REM compile the code into the bin folder
+javac  -cp ..\src -Xlint:none -d ..\bin ..\src\seedu\addressbook\Main.java
+
+REM run the program, feed commands from input.txt file and redirect the output to the actual.txt
+java -classpath ..\bin seedu.addressbook.Main < input.txt
+

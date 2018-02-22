5d4063addfeb1964487efeac9964c263b4c95e0b
USER-PC\User <ekabuyung@gmail.com>
2018-02-22 22:35:02 +0800

    Revert "update isAnyNull() Junit test"
    This reverts commit 787c81e406b5a732db6e42c0a2e96e0e930ad378.

--- test/java/seedu/addressbook/common/UtilsTest.java
+++ test/java/seedu/addressbook/common/UtilsTest.java
@@ -46,29 +46,25 @@ public class UtilsTest {
 
     @Test
     public void isAnyNull() throws Exception{
-        // empty list
-        assertFalse(Utils.isAnyNull());
+        //empty list
+        assertIsNotNull();
 
-        // Any non-empty list
-        assertFalse(Utils.isAnyNull(new Object(), new Object()));
-        assertFalse(Utils.isAnyNull("test"));
-        assertFalse(Utils.isAnyNull(""));
+        //contains null
+        assertIsNull(null);
+        //assertIsNull("a", null);
+        //assertIsNull(null, "a");
 
-        // non empty list with just one null at the beginning
-        assertTrue(Utils.isAnyNull((Object) null));
-        assertTrue(Utils.isAnyNull(null, "", new Object()));
-        assertTrue(Utils.isAnyNull(null, new Object(), new Object()));
+        //does not contain null
+        assertIsNotNull("a");
+        assertIsNotNull("a", "b", "c");
 
-        // non empty list with nulls in the middle
-        assertTrue(Utils.isAnyNull(new Object(), null, null, "test"));
-        assertTrue(Utils.isAnyNull("", null, new Object()));
+    }
 
-        // non empty list with one null as the last element
-        assertTrue(Utils.isAnyNull("", new Object(), null));
-        assertTrue(Utils.isAnyNull(new Object(), new Object(), null));
+    private void assertIsNull(Object... objects) {
+        assertTrue(Utils.isAnyNull(objects));
+    }
 
-        // confirms nulls inside the list are not considered
-        List<Object> nullList = Arrays.asList((Object) null);
-        assertFalse(Utils.isAnyNull(nullList));
+    private void assertIsNotNull(Object... objects) {
+        assertFalse(Utils.isAnyNull(objects));
     }
 }
